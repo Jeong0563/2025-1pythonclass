f333fe8b27516d0096c2c9882a00ae1d51c59d47
username <rohj2005@naver.com>
2025-04-01 11:33:57 +0900

    matplotlib

--- p01pi.py
+++ p01pi.py
@@ -1,11 +1,5 @@
 # 오일러 공식을 이용한 파이구하기
 
-n_terms = 100
-pi_approx = 1
-for i in range(1,n_terms):
-    pi_approx *= ((2*i+1)**2-1) / ((2* i + 1) **2)
-    print(pi_approx * 4)
-
 # n = 1
 # p=1
 # p *= ((2*n+1)**2-1) / ((2*n +1)**2)
@@ -14,6 +8,14 @@ for i in range(1,n_terms):
 # p *= ((2*n+1)**2-1) / ((2*n +1)**2)
 # print(p * 4)
 
+pilist = []
+
+p = 1
+for i in range(1,200):
+    p *= ((2*i+1)**2-1) / ((2* i + 1) **2)
+    pilist.append(p*4)
+    print(p * 4, ',')
+
 import matplotlib.pyplot as plt
-plt.plot([1, 2, 3, 4])
+plt.plot(pilist)
 plt.show()
